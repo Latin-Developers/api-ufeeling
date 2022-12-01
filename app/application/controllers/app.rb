5fdf5dd026fa49ebe2b6239991d4c771d094872a
César Garcia (希 賽) <cesar.garcia.mazariegos@outlook.com>
2022-12-01 10:45:22 +0800

    root route

--- app/application/controllers/app.rb
+++ app/application/controllers/app.rb
@@ -11,84 +11,90 @@ module UFeeling
     plugin :common_logger, $stderr
 
     route do |routing|
-      routing.assets # load CSS
-      response['Content-Type'] = 'text/html; charset=utf-8'
-      routing.public
-
       # [GET] /
       # Validate if the api is alive
       routing.root do
+        message = "UFeeling API v1 at /api/v1/ in #{App.environment} mode"
+
+        result_response = Representer::HttpResponse.new(
+          Response::ApiResult.new(status: :ok, message:)
+        )
+
+        response.status = result_response.http_status_code
+        result_response.to_json
       end
 
-      # [...] /categories
-      routing on 'categories' do
-        # [GET] /categories
-        # TODO
-        routing.get do
-        end
-      end
-
-      # [...] /videos/
-      routing.on 'videos' do
-        routing.is do
-          # [GET] /videos?ids=&categories=
-          # Returns the list of videos
-          # ids = list of origin ids that needs to be filter
-          # categories = list of category ids that needs to be filter
+      routing.on 'api/v1' do
+        # [...] /categories
+        routing on 'categories' do
+          # [GET] /categories
           # TODO
           routing.get do
           end
         end
 
-        # [...]  /videos/:video_origin_id
-        routing.on String do |_video_origin_id|
+        # [...] /videos/
+        routing.on 'videos' do
           routing.is do
-            # [POST]  /videos/:video_origin_id
-            # Adds a new video into the database and obtains the comments
-            # vodeo_origin_id = id of the video in youtube
-            # TODO
-            routing.post do
-            end
-
-            # [PUT]  /videos/:video_origin_id
-            # Updates the information of a videos and its comments
-            # vodeo_origin_id = id of the video in youtube
-            # TODO
-            routing.post do
-            end
-
-            # [GET]  /videos/:video_origin_id
-            # Returns the basic information of a video
-            # vodeo_origin_id = id of the video in youtube
+            # [GET] /videos?ids=&categories=
+            # Returns the list of videos
+            # ids = list of origin ids that needs to be filter
+            # categories = list of category ids that needs to be filter
             # TODO
             routing.get do
             end
           end
 
-          # [...]  /videos/:video_origin_id/comments
-          routing.on 'comments' do
-            # [GET]  /videos/:video_origin_id/comments
-            # Gets the list of comments of a video
-            # TODO
-            routing.get do
-            end
-          end
+          # [...]  /videos/:video_origin_id
+          routing.on String do |_video_origin_id|
+            routing.is do
+              # [POST]  /videos/:video_origin_id
+              # Adds a new video into the database and obtains the comments
+              # vodeo_origin_id = id of the video in youtube
+              # TODO
+              routing.post do
+              end
 
-          # [...] /videos/:video_origin_id/sentiments
-          routing.on 'sentiments' do
-            # [...] /videos/:video_origin_id/sentimens/summary
-            routing.on 'summary' do
-              # [GET] /videos/:video_origin_id/sentimens/summary
+              # [PUT]  /videos/:video_origin_id
+              # Updates the information of a videos and its comments
+              # vodeo_origin_id = id of the video in youtube
+              # TODO
+              routing.post do
+              end
+
+              # [GET]  /videos/:video_origin_id
+              # Returns the basic information of a video
+              # vodeo_origin_id = id of the video in youtube
               # TODO
               routing.get do
               end
             end
 
-            # [...] /videos/:video_origin_id/sentimens/trend
-            routing.on 'trend' do
-              # [GET] /videos/:video_origin_id/sentimens/summary
+            # [...]  /videos/:video_origin_id/comments
+            routing.on 'comments' do
+              # [GET]  /videos/:video_origin_id/comments
+              # Gets the list of comments of a video
               # TODO
               routing.get do
+              end
+            end
+
+            # [...] /videos/:video_origin_id/sentiments
+            routing.on 'sentiments' do
+              # [...] /videos/:video_origin_id/sentimens/summary
+              routing.on 'summary' do
+                # [GET] /videos/:video_origin_id/sentimens/summary
+                # TODO
+                routing.get do
+                end
+              end
+
+              # [...] /videos/:video_origin_id/sentimens/trend
+              routing.on 'trend' do
+                # [GET] /videos/:video_origin_id/sentimens/summary
+                # TODO
+                routing.get do
+                end
               end
             end
           end
