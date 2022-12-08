f5327b37e708b39c168e1da40b404607fc085c43
Julian070396 <julian07031996@gmail.com>
2022-12-08 15:56:05 +0800

    Comments

--- app/application/services/get_comments.rb
+++ app/application/services/get_comments.rb
@@ -17,12 +17,19 @@ module UFeeling
       # Get comments
 
       def get_comments(input)
-        input[:comments] = Videos::Repository::For.klass(Videos::Entity::Comment)
-          .find_video_comments(input[:video][:id])
+        comments = Videos::Repository::For.klass(Videos::Entity::Comment)
+          .find_video_comments(input[:video_id])
+          puts(comments)
+        
+        if comments
+          Success(Response::ApiResult.new(status: :ok, message: comments))
+        else
+          Failure(Response::ApiResult.new(status: :not_found, message: "Comments #{input[:video_id]} not found"))
+        end
 
-        Success(input)
-      rescue StandardError
-        Failure('Could not get video comments')
+      rescue StandardError => e
+        puts e.backtrace.join("\n")
+        Failure(Response::ApiResult.new(status: :internal_error, message: DB_ERR_MSG))
       end
     end
   end
